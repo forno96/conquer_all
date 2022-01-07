acb49d272d77b127af31b7f237cfd0bd119360c8
Francesco Fornari <francesco.fornari.96@gmail.com>
2022-01-07 19:24:24 +0100

    ADD: game board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Client } from 'boardgame.io/react';
+import { SocketIO } from 'boardgame.io/multiplayer'
+import { TicTacToe } from './Game';
+import { TicTacToeBoard } from './Board';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const TicTacToeClient = Client({
+  game: TicTacToe,
+  board: TicTacToeBoard,
+  multiplayer: SocketIO({ server: 'localhost:8000' }),
+});
+
+const App = () => (
+  <div>
+    <p>Player 0</p>
+    <TicTacToeClient playerID="0" />
+    <br/>
+    <p>Player 1</p>
+    <TicTacToeClient playerID="1" />
+    <br/>
+    <p>Spect</p>
+    <TicTacToeClient playerID="spect" />
+  </div>
+);
 
 export default App;
