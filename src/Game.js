7e4b8712227c3dac2720a90f74219862301d86f4
Francesco Fornari <francesco.fornari.96@gmail.com>
2022-01-10 10:35:54 +0100

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -44,16 +44,5 @@ export const Conquer = {
             return { draw: true };
         }
     },
-
-    ai: {
-        enumerate: (G, ctx) => {
-            let moves = [];
-            for (let i = 0; i < 9; i++) {
-                if (G.cells[i] === null) {
-                    moves.push({ move: 'clickCell', args: [i] });
-                }
-            }
-            return moves;
-        },
-    },
+    
 };
